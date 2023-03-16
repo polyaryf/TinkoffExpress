7cfd5d3ece2cda0733ef6807d22d8678b93302f7
polinom <pryftina@gmail.com>
2023-03-16 20:46:36 +0300

    added SwiftLine and minor changes

--- TinkoffExpress/TinkoffExpress/AppDelegate.swift
+++ TinkoffExpress/TinkoffExpress/AppDelegate.swift
@@ -19,7 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(
-        _ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions
+        _ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
