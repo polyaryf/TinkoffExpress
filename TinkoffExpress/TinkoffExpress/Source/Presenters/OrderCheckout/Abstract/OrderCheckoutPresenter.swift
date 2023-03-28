b1051355bbc6010a8906ecdccfdc74dc69a5d59d
polinom <pryftina@gmail.com>
2023-03-28 11:46:18 +0300

    added presenter files

---
+++ TinkoffExpress/TinkoffExpress/Source/Presenters/OrderCheckout/Abstract/OrderCheckoutPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  OrderCheckoutPresenter.swift
+//  TinkoffExpress
+//
+//  Created by Полина Рыфтина on 28.03.2023.
+//
+
+import Foundation
+
+protocol OrderCheckoutPresenter {
+    func checkoutButtonTapped()
+    func editButtonTapped()
+}
---
+++ TinkoffExpress/TinkoffExpress/Source/Presenters/OrderCheckout/Implementation/OrderCheckoutPresenterImplementation.swift
@@ -0,0 +1,21 @@
+//
+//  OrderCheckoutPresenterImplementation.swift
+//  TinkoffExpress
+//
+//  Created by Полина Рыфтина on 28.03.2023.
+//
+
+import Foundation
+
+class OrderCheckoutPresenterImplementation: OrderCheckoutPresenter {
+    
+    private weak var view: OrderCheckoutViewController?
+    
+    func checkoutButtonTapped() {
+        
+    }
+    
+    func editButtonTapped() {
+        
+    }
+}
