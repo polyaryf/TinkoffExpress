b419934413d61c32d483bd40cc8a2768b508fee9
Polya Ryftina <90066843+polyaryf@users.noreply.github.com>
2023-03-28 15:05:13 +0300

    Revert "Merge pull request #11 from polyaryf/feature/ITF-31/create-custom-button"
    This reverts commit d8a12b19639f39f814c4449407350f820aa400f8.

---
+++ TinkoffExpress/TinkoffExpress/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  TinkoffExpress
+//
+//  Created by Полина Рыфтина on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
