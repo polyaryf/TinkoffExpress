14b8fc2184cebbdec2e307ae6e5a16123e609c17
Rishat Zinatullin <108467751+Zedsfeat@users.noreply.github.com>
2023-03-28 16:14:08 +0300

    feature/ITF-001/Setup Project Structure (#14)
    * setup Project Structure
    
    * Update Podfile.lock

--- TinkoffExpress/TinkoffExpress/App/SceneDelegate.swift
+++ TinkoffExpress/TinkoffExpress/App/SceneDelegate.swift
@@ -15,10 +15,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
-        guard let windowScene = scene as? UIWindowScene else { return }
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
-        self.window = window
-        window.makeKeyAndVisible()
     }
 }
