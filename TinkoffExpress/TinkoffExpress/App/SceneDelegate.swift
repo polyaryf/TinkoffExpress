187d8d76113a2c2025d368b40c9eceda366c1cdc
Ruslan Akhmadeev <r.akhmadeev@tinkoff.ru>
2023-03-29 11:41:16 +0300

    ITF-31: Return SceneDelegate

---
+++ TinkoffExpress/TinkoffExpress/App/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  TinkoffExpress
+//
+//  Created by Полина Рыфтина on 16.03.2023.
+//
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+    }
+}
