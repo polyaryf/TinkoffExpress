609430668575323ea1ecc41e42d82d9b5fdb4d27
Ruslan Akhmadeev <akhaman@yandex.ru>
2023-03-29 13:25:57 +0300

    ITF-31: Return Button (#20)
    * ITF-31: Return Button's sources
    
    * ITF-31: Return SceneDelegate
    
    * ITF-31: Return Button's files to xcodeproj
    
    ---------
    
    Co-authored-by: Ruslan Akhmadeev <r.akhmadeev@tinkoff.ru>

--- TinkoffExpress/TinkoffExpress/App/SceneDelegate.swift
+++ TinkoffExpress/TinkoffExpress/App/SceneDelegate.swift
@@ -4,12 +4,11 @@
 //
 //  Created by Полина Рыфтина on 16.03.2023.
 //
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-    
+
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
