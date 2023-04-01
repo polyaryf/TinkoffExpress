e64396e17d2ee510ed290728d564087162b3f229
Rishat <zedsfeat@gmail.com>
2023-04-01 22:55:53 +0300

    update Coordinator

--- TinkoffExpress/TinkoffExpress/Flows/Coordinator/Coordinator.swift
+++ TinkoffExpress/TinkoffExpress/Flows/Coordinator/Coordinator.swift
@@ -9,6 +9,7 @@ import UIKit
 
 protocol Coordinator {
     var navigationController: UINavigationController? { get set }
+    var isRootCoordinator: Bool { get }
     
     func start(_ navigationController: UINavigationController)
     func move(_ assembly: Assembly, with typeOfNavigation: TypeOfNaviation)
@@ -16,10 +17,11 @@ protocol Coordinator {
 
 final class AppCoordinator: Coordinator {
     weak var navigationController: UINavigationController?
+    var isRootCoordinator = true
     
     func start(_ navigationController: UINavigationController) {
+        guard isRootCoordinator else { return }
         self.navigationController = navigationController
-        // TODO: move(CartAssembly(), with: .push)
     }
     
     func move(_ assembly: Assembly, with typeOfNavigation: TypeOfNaviation) {
@@ -29,10 +31,12 @@ final class AppCoordinator: Coordinator {
             navigationController?.pushViewController(viewController, animated: true)
         case .present:
             let childCoordinator = AppCoordinator()
+            childCoordinator.isRootCoordinator = false
             
             let viewController = assembly.createViewController(coordinator: childCoordinator)
             let childNavigationController = UINavigationController(rootViewController: viewController)
             
+            childCoordinator.start(childNavigationController)
             childCoordinator.navigationController = childNavigationController
             navigationController?.present(childNavigationController, animated: true)
         }
