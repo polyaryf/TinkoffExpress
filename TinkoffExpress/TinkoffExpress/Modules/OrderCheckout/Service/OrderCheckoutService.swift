5d8c24d99b6dde7be1ed03c8aebff97731729708
Polina Ryftina <pryftina@gmail.com>
2023-04-03 18:15:07 +0300

    added OrderCheckout module

---
+++ TinkoffExpress/TinkoffExpress/Modules/OrderCheckout/Service/OrderCheckoutService.swift
@@ -0,0 +1,12 @@
+//
+//  OrderCheckoutService.swift
+//  TinkoffExpress
+//
+//  Created by Полина Рыфтина on 03.04.2023.
+//
+
+import Foundation
+
+protocol OrderCheckoutService {}
+
+class MockOrderCheckoutService: OrderCheckoutService {}
