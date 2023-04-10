2f0e17bc5748058031f23458522f1d83e09c92f4
Polina Ryftina <pryftina@gmail.com>
2023-04-10 21:54:15 +0300

    added token and dadata url

--- TinkoffExpress/TinkoffExpress/Data/Network/Constant/NetworkConstant.swift
+++ TinkoffExpress/TinkoffExpress/Data/Network/Constant/NetworkConstant.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 
+// swiftlint:disable all
 enum NetworkConstant {
-    static let baseURL = URL(string: "http://185.204.0.180:8000")!
+    static let baseTEApiURL = URL(string: "http://185.204.0.180:8000")!
+    static let baseDaDataApiURL = URL(string: "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address")!
+    static let daDataApiKey = "5706dfdc27eb513ba142289d4cb22efc6e7ea9ec"
 }
