22b0be67a9fc25e7e73324bd5296f84bc918f6a6
Rishat <zedsfeat@gmail.com>
2023-04-23 22:26:53 +0300

    Merge MeetingAppointmentModule

---
+++ TinkoffExpress/TinkoffExpress/Modules/MeetingAppointment/Model/MeetingAppointment.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingAppointment.swift
+//  TinkoffExpress
+//
+//  Created by zedsbook on 03.04.2023.
+//
+
+import Foundation
+
+struct MeetingAppointmentDate {
+    let date: String
+}
+
+struct MeetingAppointmentTime {
+    let time: String
+    let date: String
+}
