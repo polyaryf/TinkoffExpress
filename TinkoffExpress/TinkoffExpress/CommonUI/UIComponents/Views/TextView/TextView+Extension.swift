2be73ff4db8280729c94b0e801c1e8e6c89efc04
Polina Ryftina <pryftina@gmail.com>
2023-04-24 23:19:36 +0300

    refactor clearButton

--- TinkoffExpress/TinkoffExpress/CommonUI/UIComponents/Views/TextView/TextView+Extension.swift
+++ TinkoffExpress/TinkoffExpress/CommonUI/UIComponents/Views/TextView/TextView+Extension.swift
@@ -8,6 +8,33 @@
 import UIKit
 
 extension TextView {
+    func setClearButton(superViewSize: CGRect) {
+        let clearTextButton = UIButton()
+        clearTextButton.setImage(UIImage(named: "x.cross.addressInput"), for: .normal)
+        clearTextButton.translatesAutoresizingMaskIntoConstraints = false
+        clearTextButton.tag = 111
+        clearTextButton.isHidden = self.text.isEmpty
+
+        self.addSubview(clearTextButton)
+        
+        let side: CGFloat = 16
+        let trailing: CGFloat = 12
+        let leading = superViewSize.width - side * 3 - trailing
+        clearTextButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(20)
+            $0.bottom.equalToSuperview().offset(-20)
+            $0.height.equalTo(side)
+            $0.width.equalTo(side)
+            $0.leading.equalToSuperview().offset(leading)
+            $0.trailing.equalToSuperview().offset(-trailing)
+        }
+    }
+    
+    func checkClearTextButton() {
+        let clearTextButton = self.viewWithTag(111) as? UIButton
+        clearTextButton?.isHidden = self.text.isEmpty
+    }
+    
     func setPlaceholder() {
         let placeholderLabel = UILabel()
         placeholderLabel.text = "Адрес"
