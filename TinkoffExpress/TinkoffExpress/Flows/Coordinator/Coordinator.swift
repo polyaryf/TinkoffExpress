234b4819105750971d3fa0a730eeb0f39a42fa9f
zedsbook <zedsbook@Zedsbook.local>
2023-04-28 14:00:00 +0300

    add tabBar

--- TinkoffExpress/TinkoffExpress/Flows/Coordinator/Coordinator.swift
+++ TinkoffExpress/TinkoffExpress/Flows/Coordinator/Coordinator.swift
@@ -12,7 +12,7 @@ protocol Coordinator {
     var isRootCoordinator: Bool { get }
     
     func start(_ navigationController: UINavigationController)
-    func move(_ assembly: Assembly, with typeOfNavigation: TypeOfNaviation)
+    func move(_ assembly: Assembly, with typeOfNavigation: TypeOfNavigation)
 }
 
 final class AppCoordinator: Coordinator {
@@ -22,10 +22,11 @@ final class AppCoordinator: Coordinator {
     func start(_ navigationController: UINavigationController) {
         guard isRootCoordinator else { return }
         self.navigationController = navigationController
-        move(CartAssembly(), with: .push)
+        
+        setupTabBar(assemblages: [CartAssembly(), CartAssembly()])
     }
     
-    func move(_ assembly: Assembly, with typeOfNavigation: TypeOfNaviation) {
+    func move(_ assembly: Assembly, with typeOfNavigation: TypeOfNavigation) {
         switch typeOfNavigation {
         case .push:
             let viewController = assembly.createViewController(coordinator: self)
@@ -43,5 +44,26 @@ final class AppCoordinator: Coordinator {
             let viewController = assembly.createViewController(coordinator: self)
             navigationController?.setViewControllers([viewController], animated: true)
         }
+    }
+    
+    private func setupTabBar(assemblages: [Assembly]) {
+        let tabBarController = UITabBarController()
+        var viewControllers: [UIViewController] = []
+        
+        for i in 0..<assemblages.count {
+            let vc = assemblages[i].createViewController(coordinator: self)
+            let navController = UINavigationController(rootViewController: vc)
+            navController.tabBarItem = getTabBarItem(with: i)
+            viewControllers.append(navController)
+        }
+        
+        tabBarController.viewControllers = viewControllers
+        navigationController?.setViewControllers([tabBarController], animated: true)
+    }
+    
+    private func getTabBarItem(with index: Int) -> UITabBarItem {
+        let titles: [String] = ["Корзина", "Мои заказы"]
+        let imageNames: [String] = ["cartTabBarItemImage", "myOrdersTabBarItemImage"]
+        return UITabBarItem(title: titles[index], image: UIImage(named: imageNames[index]), tag: index)
     }
 }
---
+++ TinkoffExpress/TinkoffExpress/Flows/Coordinator/TabBarItem.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarItem.swift
+//  TinkoffExpress
+//
+//  Created by zedsbook on 28.04.2023.
+//
+
+import Foundation
+
+struct TabBarItem {
+    let title: String
+    let imageName: String
+}
