e66f096861dfd2ddde07e70e319582550c549196
Polina Ryftina <pryftina@gmail.com>
2023-05-03 16:06:13 +0300

    ITF-66: added rest service

--- TinkoffExpress/TinkoffExpress/Modules/OrderCheckout/Service/OrderCheckoutService.swift
+++ TinkoffExpress/TinkoffExpress/Modules/OrderCheckout/Service/OrderCheckoutService.swift
@@ -8,10 +8,52 @@
 import Foundation
 
 protocol OrderCheckoutService {
+    func createOrder(with request: OrderCreateRequest, completion: @escaping (Result<Bool, Error>) -> Void)
     func loadItems(completion: @escaping ([OrderCheckout]?) -> Void)
 }
 
+final class RestOrderCheckoutService: OrderCheckoutService {
+    // MARK: Dependency
+    
+    private let networkService: TEApiService
+    
+    // MARK: Init
+    
+    init(networkService: TEApiService) {
+        self.networkService = networkService
+    }
+    
+    // MARK: OrderCheckoutService
+    
+    func createOrder(
+        with request: OrderCreateRequest,
+        completion: @escaping (Result<Bool, Error>) -> Void
+    ) {
+        networkService.createOrder(reqest: request) { result in
+            let newResult = result
+                .map { $0 }
+                .mapError { $0 as Error }
+            completion(newResult)
+        }
+    }
+    
+    func loadItems(completion: @escaping ([OrderCheckout]?) -> Void) {
+        let items: [OrderCheckout] = [
+            .init(
+                whatWillBeDelivered: "Посылку",
+                deliveryWhen: "Завтра с 10:00 до 12:00",
+                deliveryWhere: "Ивангород, ул. Гагарина, д. 1",
+                paymentMethod: "Картой при получении")
+        ]
+        completion(items)
+    }
+}
+
 class MockOrderCheckoutService: OrderCheckoutService {
+    func createOrder(with request: OrderCreateRequest, completion: @escaping (Result<Bool, Error>) -> Void) {
+        completion(.success(true))
+    }
+    
     func loadItems(completion: @escaping ([OrderCheckout]?) -> Void) {
         let items: [OrderCheckout] = [
             .init(
