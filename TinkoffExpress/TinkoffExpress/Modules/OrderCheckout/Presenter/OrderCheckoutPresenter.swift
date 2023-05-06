15a332de955c4880148264c421e90e4ee0444760
Polina Ryftina <pryftina@gmail.com>
2023-05-06 15:39:01 +0300

    Squashed commit of the following:
    commit 235deedfe3428725086d46d22b14ade92231fc97
    Author: Rishat Zinatullin <108467751+Zedsfeat@users.noreply.github.com>
    Date:   Sat May 6 15:30:58 2023 +0300
    
        add SettingsModule (#48)
    
        Co-authored-by: zedsbook <zedsbook@Zedsbook.local>
    
    commit bd9248b6ff706b41d23c0e5943518891a9c4c1f3
    Author: Polya Ryftina <90066843+polyaryf@users.noreply.github.com>
    Date:   Sat May 6 13:50:33 2023 +0300
    
        Feature/ITF-68/Edit order checkout (#47)
    
        * ITF-68: added rest service for MyOrders module& changed model
    
        * ITF-68: added MyOrders rest service& changed model filename to 'MyOrder'
    
        * ITF-68: added types for OrderCheckout module& empty constructor to model
    
        * ITF-68: added mapper& editing logic
    
        * ITF-68: changed MyOrders assembly but needs to refactor
    
        * ITF-68: add edit button action

--- TinkoffExpress/TinkoffExpress/Modules/OrderCheckout/Presenter/OrderCheckoutPresenter.swift
+++ TinkoffExpress/TinkoffExpress/Modules/OrderCheckout/Presenter/OrderCheckoutPresenter.swift
@@ -128,3 +128,9 @@ extension OrderCheckoutPresenter: IMyOrdersModuleOutput {
         self.item = mapper.toOrderCheckout(from: order)
     }
 }
+
+extension OrderCheckoutPresenter: IMyOrdersModuleOutput {
+    func myOrders(didCompleteWith order: MyOrder) {
+        self.item = mapper.toOrderCheckout(from: order)
+    }
+}
