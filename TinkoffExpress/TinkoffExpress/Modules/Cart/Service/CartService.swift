039f25a81385fc29c09cc5b78b72e3dad57b819e
Polina Ryftina <pryftina@gmail.com>
2023-05-08 21:40:33 +0300

    ITF-51: changed cart service

--- TinkoffExpress/TinkoffExpress/Modules/Cart/Service/CartService.swift
+++ TinkoffExpress/TinkoffExpress/Modules/Cart/Service/CartService.swift
@@ -9,46 +9,57 @@ import Foundation
 import Combine
 
 protocol ICartService {
-    var currentProductsPublisher: AnyPublisher<[Product], Never> { get }
+    var currentProductsPublisher: AnyPublisher<[CartProduct], Never> { get }
     
+    func getAll() -> [CartProduct]
     func add(product: Product)
     func remove(product: Product)
     func removeAllProducts()
 }
 
-final class CartServic: ICartService {
-    var currentProductsPublisher: AnyPublisher<[Product], Never> { fatalError() }
+final class CartService: ICartService {
+    static let shared = CartService()
     
-    func add(product: Product) {}
+    var currentProductsPublisher: AnyPublisher<[CartProduct], Never> {
+        products.eraseToAnyPublisher()
+    }
     
-    func remove(product: Product) {}
+    private var products: CurrentValueSubject<[CartProduct], Never> = .init([])
     
-    func removeAllProducts() {}
-}
-
-protocol CartService {
-    func loadItems(completion: @escaping ([Cart]?) -> Void)
-}
-
-final class MockCartService: CartService {
-    func loadItems(completion: @escaping ([Cart]?) -> Void) {
-        let items: [Cart] = [
-            .init(text: "Чайник электрический Xiaomi Mi Smart Kettle RU EAC White", imageName: "kettle"),
-            .init(text: "Чайник 2", imageName: "kettle"),
-            .init(text: "Чайник 3", imageName: "kettle"),
-            .init(text: "Чайник 4", imageName: "kettle"),
-            .init(text: "Чайник 5", imageName: "kettle"),
-            .init(text: "Чайник 6", imageName: "kettle"),
-            .init(text: "Чайник 7", imageName: "kettle"),
-            .init(text: "Чайник 8", imageName: "kettle"),
-            .init(text: "Чайник 9", imageName: "kettle"),
-            .init(text: "Чайник 10", imageName: "kettle"),
-            .init(text: "Чайник 11", imageName: "kettle"),
-            .init(text: "Чайник 12", imageName: "kettle"),
-            .init(text: "Чайник 13", imageName: "kettle"),
-            .init(text: "Чайник 14", imageName: "kettle"),
-            .init(text: "Чайник 15", imageName: "kettle")
-        ]
-        completion(items)
+    private init() {}
+    
+    func getAll() -> [CartProduct] {
+        products.value
+    }
+    
+    func add(product: Product) {
+        if !products.value.contains(where: { cartProduct in
+            cartProduct.product == product
+        }) {
+            products.value.append(CartProduct(product: product, counter: 0))
+        }
+        products.value.forEach { cartProduct in
+            if cartProduct.product == product {
+                cartProduct.counter += 1
+            }
+        }
+    }
+    
+    func remove(product: Product) {
+        products.value.forEach { cartProduct in
+            if cartProduct.product == product {
+                if cartProduct.counter > 0 {
+                    cartProduct.counter -= 1
+                } else {
+                    let id = products.value.firstIndex { $0 === cartProduct }
+                    guard let id else { return }
+                    products.value.remove(at: id)
+                }
+            }
+        }
+    }
+    
+    func removeAllProducts() {
+        products.value.removeAll()
     }
 }
