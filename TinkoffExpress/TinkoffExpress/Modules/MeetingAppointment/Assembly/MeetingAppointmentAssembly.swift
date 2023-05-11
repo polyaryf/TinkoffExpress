754d335d095735d09c7c79c8ce5bb9e968fefdc7
Polina Ryftina <pryftina@gmail.com>
2023-05-11 23:58:38 +0300

    ITF-83:added meeting appointment module behavior when edit

--- TinkoffExpress/TinkoffExpress/Modules/MeetingAppointment/Assembly/MeetingAppointmentAssembly.swift
+++ TinkoffExpress/TinkoffExpress/Modules/MeetingAppointment/Assembly/MeetingAppointmentAssembly.swift
@@ -9,9 +9,34 @@ import UIKit
 
 protocol IMeetingAppointmentAssembly {
     func createMeetingAppointmentView() -> UIViewController
+    func createMeetingAppointmentView(with model: TEApiOrder) -> UIViewController
 }
 
 final class MeetingAppointmentAssembly: IMeetingAppointmentAssembly {
+    func createMeetingAppointmentView(with model: TEApiOrder) -> UIViewController {
+        let mockService = MeetingAppointmentService(api: TEApiService())
+        let router = MeetingAppointmentRouter(
+            addressInputAssembly: AddressInputAssembly(),
+            abTestAssembly: ABTestAssembly(),
+            orderCheckoutAssembly: OrderCheckoutAssembly()
+        )
+
+        let presenter = MeetingAppointmentPresenter(
+            router: router,
+            service: mockService,
+            dateFormatter: TEDateFormatter(),
+            addressSearchType: .daData,
+            useCase: .ordering,
+            isCreatingOrder: false
+        )
+
+        let viewController = MeetingAppointmentViewController(presenter: presenter)
+        presenter.view = viewController
+        presenter.viewWillAppear(with: model)
+        router.transitionHandler = viewController
+        return viewController
+    }
+    
     func createMeetingAppointmentView() -> UIViewController {
         let mockService = MeetingAppointmentService(api: TEApiService())
         let router = MeetingAppointmentRouter(
@@ -25,7 +50,8 @@ final class MeetingAppointmentAssembly: IMeetingAppointmentAssembly {
             service: mockService,
             dateFormatter: TEDateFormatter(),
             addressSearchType: .daData,
-            useCase: .ordering
+            useCase: .ordering,
+            isCreatingOrder: true
         )
 
         let viewController = MeetingAppointmentViewController(presenter: presenter)
