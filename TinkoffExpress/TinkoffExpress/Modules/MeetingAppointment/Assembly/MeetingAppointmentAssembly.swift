f1547bb6ef798758ec7841a28053d879393be4e1
Polina Ryftina <pryftina@gmail.com>
2023-05-12 14:27:45 +0300

    ITF-84: added settings service

--- TinkoffExpress/TinkoffExpress/Modules/MeetingAppointment/Assembly/MeetingAppointmentAssembly.swift
+++ TinkoffExpress/TinkoffExpress/Modules/MeetingAppointment/Assembly/MeetingAppointmentAssembly.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 protocol IMeetingAppointmentAssembly {
     func createMeetingAppointmentView() -> UIViewController
@@ -13,6 +14,9 @@ protocol IMeetingAppointmentAssembly {
 }
 
 final class MeetingAppointmentAssembly: IMeetingAppointmentAssembly {
+    private let settingsService = SettingsService.shared
+    private var cancellable: Set<AnyCancellable> = []
+    
     func createMeetingAppointmentView(with model: TEApiOrder) -> UIViewController {
         let mockService = MeetingAppointmentService(api: TEApiService())
         let router = MeetingAppointmentRouter(
@@ -44,12 +48,11 @@ final class MeetingAppointmentAssembly: IMeetingAppointmentAssembly {
             abTestAssembly: ABTestAssembly(),
             orderCheckoutAssembly: OrderCheckoutAssembly()
         )
-
         let presenter = MeetingAppointmentPresenter(
             router: router,
             service: mockService,
             dateFormatter: TEDateFormatter(),
-            addressSearchType: .daData,
+            addressSearchType: setCurrentAddressSearchType(),
             useCase: .ordering,
             isCreatingOrder: true
         )
@@ -58,5 +61,18 @@ final class MeetingAppointmentAssembly: IMeetingAppointmentAssembly {
         presenter.view = viewController
         router.transitionHandler = viewController
         return viewController
+    }
+    
+    private func setCurrentAddressSearchType() -> MeetingAppointmentPresenter.AddressSearchType {
+        var type: MeetingAppointmentPresenter.AddressSearchType = .daData
+        settingsService.currentTogglePublisher.sink { flag in
+            if flag {
+                type = .daData
+            } else {
+                type = .abTest
+            }
+        }
+        .store(in: &cancellable)
+        return type
     }
 }
